caaca3c1379623fa43a6d1f75b3e80efc40adfdb
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2024-09-12 04:40:32 +0200

    Delete api/launchPads directory

--- api/launchPads/api.ts
+++
@@ -1,6 +0,0 @@
-import { axiosInstance } from "@/services/axios";
-
-export const fetchLaunchpads = async () => {
-  const response = await axiosInstance.get("/v4/launchpads");
-  return response.data as FetchLaunchpadsResponse;
-};
